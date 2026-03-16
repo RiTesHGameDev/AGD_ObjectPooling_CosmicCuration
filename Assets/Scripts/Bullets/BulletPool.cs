05006092e4989702239e844752017755bd06dda5
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2026-03-17 05:24:28 +0530

    Bullet Pool Script Creation
    1.Pulled Bullet Class
    2.Bullet Pool created at Player Service
    3.Bullet Pool Constructor created

---
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CosmicCuration.Bullets
+{
+	public class BulletPool
+	{
+		private BulletView bulletView;
+		private BulletScriptableObject bulletScriptableObject;
+		private List<PooledBullet> poolBullets = new List<PooledBullet>();
+		public BulletPool(BulletView _bulletView,BulletScriptableObject _bulletScriptableObject) 
+		{
+			this.bulletView = _bulletView;
+			this.bulletScriptableObject = _bulletScriptableObject;
+		}
+		public class PooledBullet 
+		{
+			public BulletController Bullet;
+			bool isUsed;
+		}
+	}
+}
