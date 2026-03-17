4f83fc5123c35c82d6cf0c9c2feefa2809ef4cc1
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2026-03-17 13:05:04 +0530

    Get Bullet method

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine.Analytics;
 
 namespace CosmicCuration.Bullets
 {
@@ -7,16 +8,38 @@ namespace CosmicCuration.Bullets
 	{
 		private BulletView bulletView;
 		private BulletScriptableObject bulletScriptableObject;
-		private List<PooledBullet> poolBullets = new List<PooledBullet>();
+		private List<PooledBullet> pooledBullets = new List<PooledBullet>();
 		public BulletPool(BulletView _bulletView,BulletScriptableObject _bulletScriptableObject) 
 		{
 			this.bulletView = _bulletView;
 			this.bulletScriptableObject = _bulletScriptableObject;
 		}
+
+		public BulletController GetBullet()
+		{
+			if(pooledBullets.Count > 0)
+			{
+				PooledBullet pooledBullet = pooledBullets.Find(item => !item.isUsed);
+				if (pooledBullet != null)
+				{
+					pooledBullet.isUsed = true;
+					return pooledBullet.bullet;
+				}
+			}
+			return CreateNewPulledBullet();
+		} 
+
+		private BulletController CreateNewPulledBullet()
+		{
+			PooledBullet pooledBullet = new PooledBullet();
+			pooledBullet.bullet = new BulletController(bulletView,bulletScriptableObject);
+			pooledBullet.isUsed = true;
+			return pooledBullet.bullet;
+		}
 		public class PooledBullet 
 		{
-			public BulletController Bullet;
-			bool isUsed;
+			public BulletController bullet;
+			public bool isUsed;
 		}
 	}
 }
