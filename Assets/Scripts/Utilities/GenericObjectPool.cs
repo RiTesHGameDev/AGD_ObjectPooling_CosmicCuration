ba7b4d82dd8101cbe2d7e31b2d50460c80a09fec
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2026-03-18 18:57:18 +0530

    Generic Object Pool implementation

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+public class GenericObjectPool<T> where T : class
+{
+    private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
+
+    protected T GetItem()
+    {
+        if(pooledItems.Count > 0)
+        {
+            PooledItem<T> item = pooledItems.Find(item => !item.isUsed);
+            if(item != null)
+            {
+                item.isUsed = true;
+                return item.Item;
+            }
+        }
+
+        return CreateNewPooledItem();
+    }
+
+    private T CreateNewPooledItem()
+    {
+        PooledItem<T> newItem = new PooledItem<T>();
+        newItem.Item = CreateItem();
+        newItem.isUsed = true;
+        pooledItems.Add(newItem);
+
+        return newItem.Item;
+    }
+
+    protected virtual T CreateItem()
+    {
+        throw new NotImplementedException("Child Class dont have implemention for CreateItem()");
+    }
+    private class PooledItem<T>
+    {
+        public T Item;
+        public bool isUsed;
+    }
+}
